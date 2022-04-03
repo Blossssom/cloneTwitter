54842a3091094b2e4b636b70c33ee0b6745210fa
Blossssom <eju2003@gmail.com>
2022-04-04 03:40:50 +0900

    add firebase Database, login with SNS, get DB data and show

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -8,6 +8,7 @@ const Home = () => {
     
     // twit db에서 데이터 리스트 호출
     const getTwits = async () => {
+        // 데이터 리스트를 풀어 state로 set -> 기존 방식보다 복잡해져 확인필요
         const getQuery = query(collection(dbService, "twitClone"));
         const querySnapshot = await getDocs(getQuery);
         querySnapshot.forEach((tData) => {
@@ -43,6 +44,7 @@ const Home = () => {
                 <input type="submit" value="twitterClone" />
             </form>
             <div>
+                {/* db에서 가져온 twits 출력 */}
                 {twits.map((v) => {
                     return(<div key={v.id}>
                         <h4>{v.twitC}</h4>
